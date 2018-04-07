c468a89b4be2f8d7c2a75a069225e33d92323f7b
VjacheslavZ <slavikov.net@mail.ru>
2018-04-07 23:02:13 +0300

    transition groups

--- transitions/src/components/CSStransition.js
+++ transitions/src/components/CSStransition.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
+import { CSSTransition } from 'react-transition-group';
 
 import '../css/App.css';
 
 class Fade extends Component{
+    state = {
+        show: false
+    };
+    showDiv = () => {
+        this.setState({
+            show: !this.state.show ? true:false
+        })
+    };
+
     render(){
         return(
             <div>
-                CSS TRANSITIONS
+                <CSSTransition
+                    in={this.state.show}
+                    timeout={200}
+                    classNames='square'
+                >
+                    <div className={`square ${this.state.show}`}>
+                        hello
+                    </div>
+                </CSSTransition>
+                <div className='showDiv'
+                     onClick={this.showDiv}>
+                    show or hide
+                </div>
             </div>
         )
     }
